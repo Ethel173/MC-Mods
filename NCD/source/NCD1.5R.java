de85582376922a022940ae13927e856c9e6b05d7
Ethel173 <CMoody173@gmail.com>
2018-02-04 17:18:27 -0600

    added new 1.12.2 branch

---
+++ NCD/source/NCD1.5R.java
@@ -0,0 +1,45 @@
+package com.Ethel.NoCreeperDestruction;
+
+import com.google.common.eventbus.Subscribe;
+
+import net.minecraft.entity.monster.EntityCreeper;
+import net.minecraft.world.Explosion;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
+@Mod(modid = NCD.MODID, name = NCD.MODNAME, version = NCD.VERSION)
+public class NCD {
+	
+public static final String MODID = "NCD";
+public static final String MODNAME = "NoCreeperDestruciton";
+public static final String VERSION = "1.5R";
+public static final String[] TIME_SINCE_IGNITED = { "timeSinceIgnited", "field_70833_d", "bq" };
+public static final String[] FUSE_TIME = { "fuseTime", "field_82225_f", "br" };
+
+@Subscribe
+public void Creeper(LivingUpdateEvent  event) {
+	EntityCreeper creeper = null;
+	if (event.getEntityLiving() instanceof EntityCreeper)
+		creeper = (EntityCreeper) event.getEntityLiving();
+	if (creeper != null && creeper.getCreeperState() == 1) {
+		int ignitedTime = ((Integer) ReflectionHelper.getPrivateValue(EntityCreeper.class, creeper,TIME_SINCE_IGNITED)).intValue();
+		int fuseTime = ((Integer) ReflectionHelper.getPrivateValue(EntityCreeper.class, creeper, FUSE_TIME)).intValue();
+		if (ignitedTime >= fuseTime - 1) {
+			creeper.setDead();
+			boom(creeper);}}}
+
+private void boom(EntityCreeper creeper) {
+	float explosionStrength = creeper.getPowered() ? 2.0F : 1.0F;
+	double posx = creeper.posX;
+	double posy = creeper.posY;
+	double posz = creeper.posZ;
+	Explosion explosion = creeper.world.createExplosion(creeper, posx, posy, posz,  3 * explosionStrength, false);
+	explosion.doExplosionA();}
+
+@EventHandler
+public void init(FMLInitializationEvent event){
+	MinecraftForge.EVENT_BUS.register(new NCD());}
+}
--- RSPP/Source/RSPP1.5R.java
+++ RSPP/Source/RSPP1.5R.java
@@ -1,32 +1,31 @@
 package com.Ethel.RedStonePerfPack;
 
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
 import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = RSPP.MODID, name = RSPP.MODNAME, version = RSPP.VERSION)
 public class RSPP {
 
     public static final String MODID = "RSPP";
     public static final String MODNAME = "RedStonePerfPack";
-    public static final String VERSION = "1.5R";
+    public static final String VERSION = "1.52R-ZLL";
 
     @EventHandler
     public void init(FMLInitializationEvent e) {
-    	Blocks.redstone_wire.setLightLevel(3);
-		Blocks.redstone_block.setLightLevel(3);
+    	Blocks.REDSTONE_WIRE.setLightLevel((float) 0.3);
 		
-		Blocks.redstone_ore.setLightLevel(0);
-		Blocks.lit_redstone_ore.setLightLevel(0);
+		Blocks.REDSTONE_ORE.setLightLevel(0);
+		Blocks.LIT_REDSTONE_ORE.setLightLevel(0);
 		
-		Blocks.redstone_torch.setLightLevel(3);
-		Blocks.unlit_redstone_torch.setLightLevel(3);
+		Blocks.REDSTONE_TORCH.setLightLevel((float) 0.3);
+		Blocks.UNLIT_REDSTONE_TORCH.setLightLevel((float) 0.3);
 		
-		Blocks.powered_repeater.setLightLevel(0);
-		Blocks.unpowered_repeater.setLightLevel(0);
+		Blocks.POWERED_REPEATER.setLightLevel((float) 0.3);
+		Blocks.UNPOWERED_REPEATER.setLightLevel((float) 0.3);
 		
-		Blocks.powered_comparator.setLightLevel(3);
-		Blocks.unpowered_comparator.setLightLevel(3);
+		Blocks.POWERED_COMPARATOR.setLightLevel((float) 0.3);
+		Blocks.UNPOWERED_COMPARATOR.setLightLevel((float) 0.3);
     }
 }
